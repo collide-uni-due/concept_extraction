ba8556cdcfeb82c82ab667201d8c0b88a9e59aaa
Yassin <yassin_taskin@web.de>
2019-04-10 13:57:59 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='concept_extraction',
+    version='0.1',
+    packages=['concept_extraction'],
+    url='',
+    license='',
+    author='Yassin Taskin',
+    author_email='',
+    description=''
+)
